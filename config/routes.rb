0f343fa0945ddb3fa1d31efa59a01b6a0479fdc4
Steven Orvis <steven@huntingpa.info>
2014-07-07 21:43:46 -0300

    added merge textbox and button to UI

--- config/routes.rb
+++ config/routes.rb
@@ -111,6 +111,7 @@ Rails.application.routes.draw do
     match "/admin/#{i}", :to => "admin/#{i}#index", :format => false
     match "/admin/#{i}(/:action(/:id))", :to => "admin/#{i}", :action => nil, :id => nil, :format => false
   end
+  post "/admin/content/merge", :to => "admin/content#merge"
 
   # default
   root :to  => 'articles#index', :format => false
